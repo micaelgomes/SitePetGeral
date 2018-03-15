1db633a38cfe2b4da7871bdf9302033010bea179
micaelgoms <micaellgoms@gmail.com>
2018-03-15 16:33:52 -0300

    correção responsividade do parallax

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <html <?php language_attributes(); ?>> 
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo( get_bloginfo( 'title' ) ); ?></title>
     <link rel="shortcut icon" href="logotitle.png">
 
