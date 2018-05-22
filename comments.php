940a58dc542a1c12085a6fa58d96eae5d5962e6a
micaelgoms <micaellgoms@gmail.com>
2018-05-22 13:02:00 -0300

    finalização da section comentarios

--- comments.php
+++ comments.php
@@ -23,7 +23,7 @@ if ( post_password_required() ) {
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h5 class="nunito">
+		<h5 class="title-comments nunito">
 			<?php
 				printf( // WPCS: XSS OK.
 					esc_html( _nx( 'Um comentário sobre &ldquo;%2$s&rdquo;', '%1$s comentários sobre &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'materialize-css' ) ),
@@ -45,14 +45,14 @@ if ( post_password_required() ) {
 			</nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
 
-		<ol class="comment-list">
+		<ul class="comment-list">
 			<?php
 				wp_list_comments( array(
-					'style'      => 'ol',
+					'style'      => 'ul',
 					'short_ping' => true,
 				) );
 			?>
-		</ol><!-- .comment-list -->
+		</ul><!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 			<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
