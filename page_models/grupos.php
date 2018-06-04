dfa04461f135a29c8857d569d6c6a973c7f2e944
micaelgoms <micaellgoms@gmail.com>
2018-06-04 10:06:22 -0300

    atualiazacao da pagina de grupos

--- page_models/grupos.php
+++ page_models/grupos.php
@@ -33,116 +33,46 @@
 					I am convenient because I require little markup to use effectively.</p>
 				</div>
 				<div class="card-reveal">
-					<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descrição do PET</span>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
+					<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Petianos</span>
+					
+					<ul class="collection">
+						<li class="collection-item avatar">
+						<img src="http://localhost/wordpress/wp-content/uploads/2018/06/capa.png" alt="" class="circle">
+						<span class="title">Title</span>
+						<p>First Line <br>
+							Second Line
+						</p>
+						<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+						</li>
+						<li class="collection-item avatar">
+						<i class="material-icons circle">folder</i>
+						<span class="title">Title</span>
+						<p>First Line <br>
+							Second Line
+						</p>
+						<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+						</li>
+						<li class="collection-item avatar">
+						<i class="material-icons circle green">insert_chart</i>
+						<span class="title">Title</span>
+						<p>First Line <br>
+							Second Line
+						</p>
+						<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+						</li>
+						<li class="collection-item avatar">
+						<i class="material-icons circle red">play_arrow</i>
+						<span class="title">Title</span>
+						<p>First Line <br>
+							Second Line
+						</p>
+						<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+						</li>
+					</ul>
+            
 				</div>
 				<div class="card-action center-align">
-					<a class="waves-effect waves-light btn button-color-grupo activator"><i class="material-icons right">add</i>Veja Mais</a>
-				</div>
-			</div>
-		</div>	
-		<!-- cardEnd -->
-		<!-- cardStart -->
-		<div class="col s12 m6 l4 xl3">
-			<div class="card z-depth-3">
-				<div class="card-image">
-					<img src="http://localhost/wordpress/wp-content/uploads/2018/03/pexels-photo-533189.jpeg">
-					<span class="card-title">Biologia</span>
-				</div>
-				<div class="card-content">
-					<p>I am a very simple card. I am good at containing small bits of information.
-					I am convenient because I require little markup to use effectively.</p>
-				</div>
-				<div class="card-reveal">
-					<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descrição do PET</span>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
-				</div>
-				<div class="card-action center-align">
-					<a class="waves-effect waves-light btn button-color-grupo activator"><i class="material-icons right">add</i>Veja Mais</a>
-				</div>
-			</div>
-		</div>	
-		<!-- cardEnd -->
-			<!-- cardStart -->
-		<div class="col s12 m6 l4 xl3">
-			<div class="card z-depth-3">
-				<div class="card-image">
-					<img src="http://localhost/wordpress/wp-content/uploads/2018/03/pexels-photo-533189.jpeg">
-					<span class="card-title">Física</span>
-				</div>
-				<div class="card-content">
-					<p>I am a very simple card. I am good at containing small bits of information.
-					I am convenient because I require little markup to use effectively.</p>
-				</div>
-				<div class="card-reveal">
-					<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descrição do PET</span>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
-				</div>
-				<div class="card-action center-align">
-					<a class="waves-effect waves-light btn button-color-grupo activator"><i class="material-icons right">add</i>Veja Mais</a>
-				</div>
-			</div>
-		</div>	
-		<!-- cardEnd -->
-		<!-- cardStart -->
-		<div class="col s12 m6 l4 xl3">
-			<div class="card z-depth-3">
-				<div class="card-image">
-					<img src="http://localhost/wordpress/wp-content/uploads/2018/03/pexels-photo-533189.jpeg">
-					<span class="card-title">Direito Humanos</span>
-				</div>
-				<div class="card-content">
-					<p>I am a very simple card. I am good at containing small bits of information.
-					I am convenient because I require little markup to use effectively.</p>
-				</div>
-				<div class="card-reveal">
-					<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descrição do PET</span>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
-				</div>
-				<div class="card-action center-align">
-					<a class="waves-effect waves-light btn button-color-grupo activator"><i class="material-icons right">add</i>Veja Mais</a>
-				</div>
-			</div>
-		</div>	
-		<!-- cardEnd -->
-				<!-- cardStart -->
-		<div class="col s12 m6 l4 xl3">
-			<div class="card z-depth-3">
-				<div class="card-image">
-					<img src="http://localhost/wordpress/wp-content/uploads/2018/03/pexels-photo-533189.jpeg">
-					<span class="card-title">outro</span>
-				</div>
-				<div class="card-content">
-					<p>I am a very simple card. I am good at containing small bits of information.
-					I am convenient because I require little markup to use effectively.</p>
-				</div>
-				<div class="card-reveal">
-					<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descrição do PET</span>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
-				</div>
-				<div class="card-action center-align">
-					<a class="waves-effect waves-light btn button-color-grupo activator"><i class="material-icons right">add</i>Veja Mais</a>
-				</div>
-			</div>
-		</div>	
-		<!-- cardEnd -->
-					<!-- cardStart -->
-		<div class="col s12 m6 l4 xl3">
-			<div class="card z-depth-3">
-				<div class="card-image">
-					<img src="http://localhost/wordpress/wp-content/uploads/2018/03/pexels-photo-533189.jpeg">
-					<span class="card-title">outro</span>
-				</div>
-				<div class="card-content">
-					<p>I am a very simple card. I am good at containing small bits of information.
-					I am convenient because I require little markup to use effectively.</p>
-				</div>
-				<div class="card-reveal">
-					<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descrição do PET</span>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
-				</div>
-				<div class="card-action center-align">
-					<a class="waves-effect waves-light btn button-color-grupo activator"><i class="material-icons right">add</i>Veja Mais</a>
+					<a class="waves-effect waves-light btn button-color-grupo activator"><i class="material-icons right"></i>Integrantes</a>
 				</div>
 			</div>
 		</div>	
