60a21b7d3180e866a18eb6141ab8287347ef1482
micaelgoms <micaellgoms@gmail.com>
2018-09-17 11:21:09 -0300

    adicionando texto na pagina de apresentacao

---
+++ template/grupos.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * template de mostragem de resultados
+ * Template Name: grupos
+ *
+ * @package SiteGeralPETUFMA
+ * @since SiteGeralPETUFMA 0.1
+ *
+ */
+?>
+
+<?php get_header(); ?>
+<?php $URL = 'https://pet.ufma.br/' ?>
+<?php //$URL = 'http://localhost/wordpress/' ?>
+
+<?php get_footer(); ?>
